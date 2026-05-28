cf43e89f0c2a75783df393f171284311329531cc
papasmf1 <papasmf1@gmail.com>
2026-05-28 13:41:08 +0900

    웹크롤링 연습2

--- 오늘의유머.py
+++ 오늘의유머.py
@@ -10,28 +10,27 @@ import re
 hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
 
 #파일에 저장(write text) 
-f = open("clien.txt", "wt", encoding="utf-8")
+f = open("todayHumor.txt", "wt", encoding="utf-8")
 
-for i in range(0,10):
-    url = "https://www.clien.net/service/board/sold?&od=T31&category=0&po=" + str(i)
+for i in range(1, 11):
+    url = "https://www.todayhumor.co.kr/board/list.php?table=bestofbest&page=" + str(i)
     print(url) 
     #웹브라우져 헤더 추가 
     req = urllib.request.Request(url, headers = hdr)
     data = urllib.request.urlopen(req).read()
     soup = BeautifulSoup(data, "html.parser")
     #필터링 작업 
-    list = soup.find_all("span", {"data-role":"list-title-text"})
+    list = soup.find_all("td", attrs={"class":"subject"})
     for tag in list:
-        title = tag.text.strip()
-        print(title)
+        title = tag.find("a").text.strip()
         #문자열 검색(정규표현식)
-        # if re.search("아이폰", title):
-        #     print(title)
-        #     f.write(title + "\n")
+        if re.search("일본", title):
+            print(title)
+            f.write(title + "\n")
     
 f.close()
 
-#선택한 블럭 주성: ctrl + / 
-# <span class="subject_fixed" data-role="list-title-text" title="미개봉) 아이폰 Xs 데빌케이스 Type X2">
-# 				미개봉) 아이폰 Xs 데빌케이스 Type X2
-# </span>
+#<td class="subject">
+#<a href="/board/view.php?table=bestofbestno=482962>
+# "요즘 시대에서 한국인을 나누는 기준"
+# </a>
